7567ce3abbc5d0d2996426e6813b409a09199089
fabianomoraes <moraes_18@hotmail.com>
2022-05-14 23:23:10 -0300

    Segundo Commit

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <span className="text-danger">Welcome</span> to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
