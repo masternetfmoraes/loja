7126a481e78c7ac93b26e926125c6ed5721f6480
fabianomoraes <moraes_18@hotmail.com>
2022-05-14 23:47:24 -0300

    menu Left

--- pages/components/MenuLeft.jsx
+++ pages/components/MenuLeft.jsx
@@ -1,9 +1,34 @@
-import { Button } from 'react-bootstrap';
+import { Button,Offcanvas } from 'react-bootstrap';
+import React, { useEffect, useState } from "react";
 
-export default function MenuLeft(){
+function MenuLeft(){
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return(
         <>
-        <Button variant="success" className="m-5">Button #3</Button>
+        <Button variant="primary" className="float-end" onClick={handleShow}>
+        Lance
+      </Button>
+
+      <Offcanvas show={show} onHide={handleClose}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          Some text as placeholder. In real life you can have the elements you
+          have chosen. Like, text, images, lists, etc.
+        </Offcanvas.Body>
+        <Button variant="primary" className="float-end" onClick={handleShow}>
+        Lance
+      </Button>
+      </Offcanvas>
         </>
     )
 }
+
+export default MenuLeft;
+
+//render(<Example />);
