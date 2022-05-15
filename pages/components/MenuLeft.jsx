a377308e41311a5f99a63edab7d0e49c2236ead9
fabianomoraes <moraes_18@hotmail.com>
2022-05-14 23:33:34 -0300

    Menu Left

---
+++ pages/components/MenuLeft.jsx
@@ -0,0 +1,9 @@
+import { Button } from 'react-bootstrap';
+
+export default function MenuLeft(){
+    return(
+        <>
+        <Button variant="success" className="m-5">Button #3</Button>
+        </>
+    )
+}
