d67d2026a37af9d438067b05be7e0bf9d7762cc2
fabianomoraes <moraes_18@hotmail.com>
2022-05-15 00:07:49 -0300

    menu top ms-auto

--- pages/components/MenuTop.jsx
+++ pages/components/MenuTop.jsx
@@ -8,7 +8,7 @@ export default function MenuTop(){
     <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
+      <Nav className="ms-auto">
         <Nav.Link href="#home">Home</Nav.Link>
         <Nav.Link href="#link">Link</Nav.Link>
         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
